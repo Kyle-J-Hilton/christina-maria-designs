49975111619327deb52d22bd8409321e38b34b9f
Kyle-J-Hilton <106459128+Kyle-J-Hilton@users.noreply.github.com>
2023-03-13 13:59:41 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,19 +14,19 @@ const Home = ({ products, bannerData }) => {
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/favicon.png"
+          href="/favicon.ico"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon.png"
+          href="/favicon.ico"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon.png"
+          href="/favicon.ico"
         />
 
         <meta name="theme-color" content="#ffffff" />
